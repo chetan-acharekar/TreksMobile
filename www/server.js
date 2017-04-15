493b0b20480ebfad571c8299d7907ae441867048
chetan <acharekarchetan4@gmail.com>
2017-04-15 19:43:27 +0530

    compression and caching added

--- www/server.js
+++ www/server.js
@@ -1,3 +1,11 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8081)
+var express = require('express');
+var app = express();
+var compression = require('compression');
+
+app.use(compression())
+
+app.use(express.static(__dirname,{maxAge:86400000 }))
+
+app.listen(8081, function () {
+  console.log('Example app listening on port 8081!')
+})
